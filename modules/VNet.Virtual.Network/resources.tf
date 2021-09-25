c6e3cd800d301b2488df38406ee183edf0e5d873
FMingo <fmingo@kabel.es>
2021-09-25 18:02:04 +0200

    error

--- modules/VNet.Virtual.Network/resources.tf
+++ modules/VNet.Virtual.Network/resources.tf
@@ -1,6 +1,6 @@
 resource "azurerm_virtual_network" "AVDVNETSPK02"{
     name = var.vnet_name
-    location = var.
+    location = var.azure_location
     resource_group_name = azurerm_resource_group.RGAVDSPK02.name
     address_space = ["192.168.101.0/26"]
     depends_on = [
