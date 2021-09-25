0480ba6fc5767c8712f805465676d485b064bf23
FMingo <fmingo@kabel.es>
2021-09-25 23:08:25 +0200

    B-2

--- modules/VNet.Virtual.Network/resources.tf
+++ modules/VNet.Virtual.Network/resources.tf
@@ -1,15 +1,12 @@
 
 resource "azurerm_resource_group" "rgvnet01" {
   name = var.resource_group_name
-  location = local.azure_location
-
-  tags = {
-    var.tags
-  }  
+  location = var.azure_location
+  tags = var.tags
 
 }
 
-resource "azurerm_virtual_network" "AVDVNETSPK02"{
+resource "azurerm_virtual_network" "vnet"{
     name = var.vnet_name
     location = var.azure_location
     resource_group_name = var.resource_group_name
@@ -17,8 +14,17 @@ resource "azurerm_virtual_network" "AVDVNETSPK02"{
     depends_on = [
         azurerm_resource_group.rgvnet01
   ]
-  tags = {
-    var.tags
-  }
+  tags = var.tags
+}
+
+
+resource "azurerm_subnet" "subnet" {
+    name                 = var.vnet_subnet_name
+    resource_group_name  = azurerm_resource_group.rgvnet01
+    virtual_network_name = azurerm_virtual_network.vnet
+    address_prefixes       = var.vnet_subnet_address_space
+    depends_on = [
+        azurerm_virtual_network.vnet
+    ]
 
 }
--- modules/VNet.Virtual.Network/variables.tf
+++ modules/VNet.Virtual.Network/variables.tf
@@ -35,32 +35,32 @@ variable "provider"{
 }
 
 variable "environment" {
-  type          = string
-  description   = "environment short name"
-  default       = "dev"
+  type              = string
+  description       = "environment short name"
+  default           = "dev"
   validation {
-    condition     = contains(["dev", "pre", "pro", "int"], var.environment)
-    error_message = "(Required) The environment specified must be one of the allowed values (dev, pre, pro, int)."
+    condition       = contains(["dev", "pre", "pro", "int"], var.environment)
+    error_message   = "(Required) The environment specified must be one of the allowed values (dev, pre, pro, int)."
   }
 }
 
 variable "region"{
-    type        = string
-    description = "Azure region"
-    default     = "euwe"
+    type            = string
+    description     = "Azure region"
+    default         = "euwe"
 }
 
 variable "services" {
-    type        = string
-    description = "Service to which the resource belongs"
+    type            = string
+    description     = "Service to which the resource belongs"
 }
 
 variable "resource"{
-    type        = string
-    description = "Allowed resource nomenclature"
-    default     =  "vnet"
+    type            = string
+    description     = "Allowed resource nomenclature"
+    default         =  "vnet"
     validation {
-      condition = contains (["nsg","exr","exg","vpng","vnet","nic","snet","rt","pip","fw","adf","stg","dl","adb","sqlser","sqldb","apn","azr","aml","kv","lga"],var.resource)
+      condition     = contains(["nsg","exr","exg","vpng","vnet","nic","snet","rt","pip","fw","adf","stg","dl","adb","sqlser","sqldb","apn","azr","aml","kv","lga"], var.resource)
       error_message = "The allowed values for resource type are: nsg,exr,exg,vpng,vnet,nic,snet,rt,pip,fw,adf,stg,dl,adb,sqlser,sqldb,apn,azr,aml,kv,lga "
     }
 }
@@ -68,18 +68,23 @@ variable "resource"{
 #Varialbe for VNET resource
 
 variable "vnet_name"{
-    type        =string
-    description = "Name of the vnet resource"
-    default     = join("-",[var.company,var.provider,var.environment,var.region,"net",var.resource])
+    type            = string
+    description     = "Name of the vnet resource"
+    default         = join("-",[var.company,var.provider,var.environment,var.region,"net",var.resource])
 }
 
 variable "vnet_address_space"{
-    type=string
-     description = "CIDR address space for the VNET"
-     default = "10.10.0.0/16"
+    type            = string
+     description    = "CIDR address space for the VNET"
+     default        = "10.10.0.0/16"
 }
 
- variable "vnet_subnet"{
+variable "vnet_subnet_name" {
+    type            = string
+    description     = "(Required) Subnet Name."
+}
+
+ variable "vnet_subnet_address_space"{
      type=string
      description = "CIDR address space for the subnet"
      default = "10.10.0.0/24"
